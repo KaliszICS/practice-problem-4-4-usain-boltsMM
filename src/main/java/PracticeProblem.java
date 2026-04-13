fe6ae4fa4e2602d9aa45ebf08f56f1a5b98f54fc
usain-boltsMM <343421046@tdsb.ca>
2026-04-13 20:05:33 +0000

    casanova

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,24 @@ public class PracticeProblem {
 
 	}
 
+	public static boolean hasCapital(String cap){
+		return !cap.equals(cap.toLowerCase());
+	}
+
+	public static boolean isPrime(int num){
+		boolean brg= false;
+		if (num <= 1) {
+			return false;
+		}
+
+	for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+	}
+
 }
+
